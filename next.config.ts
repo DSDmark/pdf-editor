fa09b2934edc57de111574fcb8217d8033d60bc1
Sanskar Sahu <sahup3296@gmail.com>
2025-05-10 17:55:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: "loose",
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /pdf\.worker\.min\.(js|mjs)$/,
